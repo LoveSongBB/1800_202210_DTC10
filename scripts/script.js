35c120114444592a746af348f34521a9185ea098
Changwhi Oh <whinada@gmail.com>
2022-03-11 14:00:12 -0800

    Test push

--- scripts/script.js
+++ scripts/script.js
@@ -148,22 +148,22 @@ $(document).ready(function(){
     var day = (now.getDate()) > 9 ? ''+(now.getDate()) : '0'+(now.getDate());           
     //년도 selectbox만들기               
     for(var i = 1900 ; i <= year ; i++) {
-        $('#year').append('<option value="' + i + '">' + i + '년</option>');    
+        $('#country').append('<option value="' + i + '">' + i + '</option>');    
     }
 
     // 월별 selectbox 만들기            
     for(var i=1; i <= 12; i++) {
         var mm = i > 9 ? i : "0"+i ;            
-        $('#month').append('<option value="' + mm + '">' + mm + '월</option>');    
+        $('#state').append('<option value="' + mm + '">' + mm + '</option>');    
     }
     
     // 일별 selectbox 만들기
     for(var i=1; i <= 31; i++) {
         var dd = i > 9 ? i : "0"+i ;            
-        $('#day').append('<option value="' + dd + '">' + dd+ '일</option>');    
+        $('#day').append('<option value="' + dd + '">' + dd+ '</option>');    
     }
-    $("#year  > option[value="+year+"]").attr("selected", "true");        
-    $("#month  > option[value="+mon+"]").attr("selected", "true");    
+    $("#country  > option[value="+year+"]").attr("selected", "true");        
+    $("#state  > option[value="+mon+"]").attr("selected", "true");    
     $("#day  > option[value="+day+"]").attr("selected", "true");       
   
 })
