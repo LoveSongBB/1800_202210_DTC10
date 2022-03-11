4b94480d0be0f62a5a099da1146805c3b5c4d9da
Bryan <bryant.business@hotmail.com>
2022-03-11 11:26:05 -0800

    SPRINT 2 COMPLETE

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ function insertName(){
         if (user){
             currentUser = db.collection("users").doc(user.uid);
             currentUser.get().then(userDoc =>{
-
+            console.log(currentUser)
                 var user_Name = userDoc.data().name;
                 console.log(user_Name);
                 document.getElementById("name-main").innerHTML=user_Name;
