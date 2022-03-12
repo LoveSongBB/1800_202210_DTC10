342c7c7088b85e546f54b1960797198df35d85c0
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2022-03-11 22:13:27 -0800

    fixed login and chat function

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,19 +1,38 @@
 // send message function
 
-function calculator(){
-    text = $("#textmessage").val();
-    result = text
-    styled_result = '<div class="message message-out">'+'<a href="#" data-bs-toggle="modal" data-bs-target="#modal-profile" class="avatar avatar-responsive">'+'<img class="avatar-img" src="assets/img/avatars/1.jpg" alt="">'+'</a>'+'<div class="message-inner">'+'<div class="message-body">'+'<div class="message-content">'+'<div class="message-text">'+'<p class="firebase_stored">'+ result+'</p>'+'</div>'+'</div>'+'</div>'+'</div>'+'</div>'
+function calculator() {
+    result = $("#textmessage").val();
+    console.log(result)
+    styled_result = '<div class="message message-out">' + '<a href="#" data-bs-toggle="modal" data-bs-target="#modal-profile" class="avatar avatar-responsive">' + '<img class="avatar-img" src="assets/img/avatars/1.jpg" alt="">' + '</a>' + '<div class="message-inner">' + '<div class="message-body">' + '<div class="message-content">' + '<div class="message-text">' + '<p class="firebase_stored">' + result + '</p>' + '</div>' + '</div>' + '</div>' + '</div>' + '</div>'
     old_div_content = jQuery('#chatmain').html();
     new_div_content = old_div_content + styled_result;
-    jQuery('#chatmain').html(new_div_content); 
+    jQuery('#chatmain').html(new_div_content);
 
-// Firebase
+    // Firebase
+    
+    // var sendmessages = db.collection("chattest").doc("chattest");
+    
+    // sendmessages.add({
+    //     chattestlist: text});
+    
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            var currentUser = db.collection("users").doc(user.uid)
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    db.collection("chattest").add({
+                        chattestlist: result
+                    }).then(() => {
+                        // window.location.href = "main.html";
+                        console.log("then() performed")
+                    })
+                })
 
-    var sendmessages = db.collection("chattest").doc("chattest");
-    sendmessages.add({
-        chattestlist: text});
-
+        } else {
+            // No user is signed in.
+        }
+    });
 }
 
 // send message function
@@ -26,7 +45,7 @@ function calculator(){
 
 
 // Firebase data base
-function setup(){
+function setup() {
     console.log("document was called")
     $("body").on("click", "#sendmessage", calculator)
 
