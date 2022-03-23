9405563bf724c4785fd349d83d390fbad18ac195
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2022-03-23 13:29:28 -0700

    Continue working on user matching

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
-function insertName(){
+function insertName() {
     // check if logged in
-    firebase.auth().onAuthStateChanged(user =>{
-        if (user){
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
             currentUser = db.collection("users").doc(user.uid);
-            currentUser.get().then(userDoc =>{
-            console.log(currentUser)
+            currentUser.get().then(userDoc => {
+                console.log(currentUser)
                 var user_Name = userDoc.data().name;
                 console.log(user_Name);
-                document.getElementById("name-main").innerHTML=user_Name;
+                document.getElementById("name-main").innerHTML = user_Name;
                 // $("#name-main").text(user_Name);
             })
             console.log(user.uid); // give id who user who logged in
--- scripts/user-matching.js
+++ scripts/user-matching.js
@@ -1,75 +1,34 @@
-function matchUser() {
-    var userMatchingAnswerListObject = {};
+var currentUser;
 
-    var userMatchingAnswerListMap = new Map();
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        currentUser = db.collection("users").doc(user.uid); // Global
+        console.log(`currentUser = ${currentUser}`);
 
-    // firebase.auth().onAuthStateChanged(userLoggedIn => {
-    //     // Check if user is logged in.
-    //     if (userLoggedIn) {
-    //         // Loop through all the users in the "users" collection in firebase
-    //         const querySnapshot = await getDocs(db.collection("users"));
-    //         querySnapshot.forEach((user) => {
-    //             // doc.data() is never undefined for query doc snapshots
-    //             console.log(user.id, " => ", user.data().answerList);
-    //             // userMatchingAnswerListMap.set(string(user.id), user.data().answerList);
-    //         });
-    //     } else {
-    //         // No one is logged in.
-    //         console.log("No one is logged in");
-    //     }
-    // })
+        // Add functions that you want to run when user is logged in
+        matchUser();
+        // console.log(getCurrentUserAnswerList(currentUser));
+    } else {
+        console.log("No one is signed in.");
+        window.location.href = '/login.html';
+    }
+})
 
-    // Loop through users collection in firebase
-    // db.collection("users").get().then(function (querySnapshot) {
-    //     querySnapshot.forEach(function (user) {
-    //         // console.log(user.id, " => ", user.data().answerList);
-    //         userMatchingAnswerListObject[String(user.id)] = user.data().answerList;
-    //     });
-    // });
-
-    console.log(userMatchingAnswerListMap);
-
-    // Loop through userMatchingAnswerListObject and compare the logged in users answerList with the other answerLists stored in firebase
-    // var matchCounter = 0;
-
-    // firebase.auth().onAuthStateChanged(user => {
-    //     // check if user is logged in.
-    //     if (user) {
-    //         console.log(`user.uid = ${user.uid}`);
-    //         // For some reason, the line below returns underfined. Don't know why.
-    //         // console.log(`userMatchingAnswerListObject[user.uid] = ${userMatchingAnswerListObject[String(user.uid)]}`); 
-    //         var loggedInUserAnswerList = userMatchingAnswerListObject[String(user.uid)];
-    //         console.log(`loggedInUserAnswerList = ${loggedInUserAnswerList}`);
-    //     } else {
-    //         // No one is logged in.
-    //         console.log("No user logged in");
-    //     }
-    // })
-
-    // The code below regarding testObject is just a test. Doesn't really do anything.
-    testMap = new Map();
-
-    testMap.set('a', 1);
-    testMap.set('b', 2);
-    testMap.set('c', 3);
-    console.log(testMap);
-    console.log(`testMap.get('a') = ${testMap.get('a')}`);
-}
-
-function getAnswerList(userID) {
-    // userID should be the user UID in string format of the user you need to look for.
-    var userToSearch = db.collection("users").doc(userID);
-    userToSearch.get().then(userDoc => {
-        var userAnswerList = userDoc.data().answerList;
-        console.log(`userAnswerList = ${userAnswerList}`);
+// TODO get this function to work.
+function getCurrentUserAnswerList(currentUserParameter) {
+    db.collection("users").get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (user) {
+            if (currentUser == user) {
+                return user.data().answerList;
+            }
+        })
     })
 }
 
-// Compares the logged in users answerList with another answerList and returns the integer amount of matched numbers they have.
+// Compares answerList of currentUser with answerList of every other user.
+// Return the integer amount of matches between the two arrays.
 function compareAnswerLists(yourUserAnswerList, otherUserAnswerList) {
     var matchCounter = 0;
-    const array1 = ['1', '2', '3', '4'];
-    const array2 = ['3', '4', '5', '6'];
 
     for (const element of yourUserAnswerList) {
         if (otherUserAnswerList.includes(element)) {
@@ -77,7 +36,37 @@ function compareAnswerLists(yourUserAnswerList, otherUserAnswerList) {
         }
     }
 
-    console.log(matchCounter);
+    console.log(`matchCounter = ${matchCounter}`);
+    return matchCounter;
+}
+
+function matchUser() {
+    var userMatches = {};
+
+    // Loop through users collection in firebase
+    db.collection("users").get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (user) {
+            console.log(user);
+            var userAnswerList = user.data().answerList
+            var userID = user.id;
+
+            console.log(userID, " => ", userAnswerList);
+            userMatches[userID] = compareAnswerLists(['1', '3', '6', '10', '13'], userAnswerList);
+        });
+    });
+
+    console.log(userMatches);
+    console.log(Object.values(userMatches));
+
+    var testObject = {
+        'a': 1,
+        'b': 2
+    }
+
+    console.log(testObject);
+    // TODO: obtain the max value in userMatches.
+
+    // TODO: get the first 5 userID's that equal to the max value in userMatches.
 }
 
 function setup() {
