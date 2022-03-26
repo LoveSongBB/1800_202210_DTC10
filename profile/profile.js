8c31d42c2649eaf9989726e2584b1aee61c0e80a
Bryan <bryant.business@hotmail.com>
2022-03-25 22:40:44 -0700

    Connect edit-profile and profile.js to firestore

--- profile/profile.js
+++ profile/profile.js
@@ -57,3 +57,22 @@ function saveUserInfo() {
                 })
     document.getElementById('personalInfoFields').disabled = true;
 }
+
+
+function insertName() {s
+    // check if logged in
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid);
+            currentUser.get().then(userDoc => {
+                console.log(currentUser)
+                var user_Name = userDoc.data().name;
+                console.log(user_Name);
+                document.getElementById("name-profile").innerHTML = user_Name;
+                // $("#name-main").text(user_Name);
+            })
+            console.log(user.uid); // give id who user who logged in
+        }
+    })
+}
+insertName();
