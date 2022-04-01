8de5bbe89344cdb6549dbdf73b7eded19b942021
bryantran-van <bryant.business@hotmail.com>
2022-04-01 06:42:32 -0700

    Add code for adding friend

--- otherUserProfile/other-user-profile.js
+++ otherUserProfile/other-user-profile.js
@@ -18,9 +18,28 @@
 //function setup() {
 //    populateDataFields();
 //}
+var currentUser;
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        currentUser = db.collection("users").doc(user.uid);   //global
+        console.log(currentUser);
+
+        // the following functions are always called when someone is logged in
+
+
+
+    } else {
+        // No user is signed in.
+        console.log("No user is signed in");
+        window.location.href = "login.html";
+    }
+});
+
+
 
 function showDetails() {
     // create a URL object
+//    let testtest = document.getElementById("test");
     let params = new URL(window.location.href);
     let id = params.searchParams.get("id");                   //parse "id"
     let hikeName = params.searchParams.get("hikeName");   //parse "collection"
@@ -31,17 +50,20 @@ function showDetails() {
     document.getElementById("HikeName").innerHTML = hikeName;
     document.getElementById("details-go-here").innerHTML = message;
 
-
-
-
     db.collection("users").where("userID", '==', id)
     .get()
     .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
+//            let testCard = testtest.content.cloneNode(true);
             // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
+                var hikeID = doc.data().userID;
+                console.log(doc.id, " => ", doc.data());
                 var biog = doc.data().bio;
-    document.getElementById("bio-div").innerHTML = biog;
+                document.getElementById("bio-div").innerHTML = biog;
+//                testCard.querySelector('i').id = 'save-' + hikeID;
+//                // this line will call a function to save the hikes to the user's document
+//                testCard.querySelector('i').onclick = () => saveBookmark(hikeID);
+
         });
     })
     .catch((error) => {
@@ -53,18 +75,14 @@ showDetails();
 
 
 
-
-
-
-
 function saveBookmark(hikeID) {
     currentUser.set({
-            bookmarks: firebase.firestore.FieldValue.arrayUnion(hikeID)
+            friendlist: firebase.firestore.FieldValue.arrayUnion(hikeID)
         }, {
             merge: true
         })
         .then(function () {
-            console.log("bookmark has been saved for: " + currentUser);
+            console.log("Add a friend: " + currentUser);
             var iconID = 'save-' + hikeID;
             //console.log(iconID);
             document.getElementById(iconID).innerText = 'bookmark';
@@ -76,4 +94,34 @@ function setHikeData(userID){
 }
 
 
-//$(document).ready(setup);
+
+
+function addFriend(){
+    let hikeCardTemplate = document.getElementById("hikeCardTemplate");
+    let hikeCardGroup = document.getElementById("hikeCardGroup");
+        let params = new URL(window.location.href);
+    let id = params.searchParams.get("id");
+    db.collection("users").where("userID", '==', id)
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+                var hikeName = doc.data().name; //gets the name field
+                var hikeID = doc.data().userID; //gets the unique ID field
+                let testHikeCard = hikeCardTemplate.content.cloneNode(true);
+                testHikeCard.querySelector('i').id = 'save-' + hikeID;
+                // this line will call a function to save the hikes to the user's document
+                testHikeCard.querySelector('i').onclick = () => saveBookmark(hikeID);
+                hikeCardGroup.appendChild(testHikeCard);
+            })
+        })
+}
+
+addFriend();
+
+function setup(){
+
+   $("#test2").click(addFriend);
+}
+
+
+$(document).ready(setup);
